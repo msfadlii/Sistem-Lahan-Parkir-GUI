fde115116f0a7307586bf5a91aa075fbc3043645
Admin <msfadliii@gmail.com>
2024-01-17 16:32:06 +0700

    17 Januari - upd GUI Main - Insert Parkir (clear)

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,4 +1,55 @@
 package Controller;
 
+import Entity.Admin;
+import Model.LoginModel;
+
+import java.util.ArrayList;
+
 public class LoginController {
+    LoginModel loginModel;
+
+    public LoginController(){
+        loginModel = new LoginModel();
+    }
+
+    public boolean cekLogin(String username, String password){
+        ArrayList<Admin> list = loginModel.listAdmin();
+
+        for (Admin admin:list) {
+            if(username.equals(admin.getUsername())){
+                if(password.equals(admin.getPassword())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean insertAdmin(String username, String password, String nama){
+        if(loginModel.searchAdmin(username) != -1){
+            return false;
+        }else{
+            loginModel.insert(username, password, nama);
+            return true;
+        }
+    }
+
+    public boolean deleteAdmin(String username){
+        if(loginModel.searchAdmin(username) != -1){
+            return false;
+        }else{
+            loginModel.delete(username);
+            return true;
+        }
+    }
+
+    public String getNama(String username){
+       ArrayList<Admin> list = loginModel.listAdmin();
+        for (Admin admin:list) {
+            if (admin.getUsername().equals(username)) {
+                return admin.getNama();
+            }
+        }
+        return "";
+    }
 }
--- src/View/Parkir/Main.java
+++ src/View/Parkir/Main.java
@@ -1,96 +1,101 @@
 package View.Parkir;
 
+import Controller.ParkirController;
+import View.Login.Login;
+import com.github.lgooddatepicker.components.DateTimePicker;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Main extends JFrame implements ActionListener {
-    JLabel titleLabel, unameLabel, passLabel, message;
-    JTextField usernameField;
-    JPasswordField passwordField;
-    JButton submitButton;
-    JCheckBox showPassword;
+public class Main extends JFrame {
+    JLabel judulLabel, platLabel, tglLabel, menu, namaAdmin;
+    JTextField platField;
+    DateTimePicker dateTimePicker;
+    JButton submitButton, menuParkir, menuListAdmin, menuListParkir, logoutButton;
+    JPanel panel;
 
+    ParkirController parkirController = new ParkirController();
     public void setFrame(){
-        setSize(600, 400);
+        setSize(800, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setLocationRelativeTo(null);
-        setTitle("Login Admin");
+        setTitle("Sistem Lahan Parkir");
         component();
         setVisible(true);
     }
 
     public void component(){
-        titleLabel = new JLabel("Aplikasi Lahan Parkir");
-        titleLabel.setBounds(200, 20, 250, 30);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        panel = new JPanel();
+        panel.setBounds(0,0, 150, 400);
+        panel.setBackground(Color.WHITE);
+        panel.setLayout(null);
 
-        unameLabel = new JLabel("Username : ");
-        unameLabel.setBounds(170, 100, 150, 30);
-        usernameField = new JTextField();
-        usernameField.setBounds(170, 130, 250, 30 );
+        menu = new JLabel("=== Menu ===");
+        menu.setBounds(35, 10, 100, 20);
 
-        passLabel = new JLabel("Password : ");
-        passLabel.setBounds(170, 160, 150, 30);
-        passwordField = new JPasswordField();
-        passwordField.setBounds(170, 190, 250, 30);
+        namaAdmin = new JLabel("Halo, Admin " + Login.nama);
+        namaAdmin.setBounds(25, 40, 150, 20);
 
-        showPassword = new JCheckBox("Show Password");
-        showPassword.setBounds(170, 220, 130, 20);
-        showPassword.addActionListener(this);
+        //list menu
+        menuParkir = new JButton("Input Parkir");
+        menuParkir.setBounds(25, 80, 100, 20);
+        menuListParkir = new JButton("List Parkir");
+        menuListParkir.setBounds(25, 120, 100, 20);
+        menuListAdmin = new JButton("List Admin");
+        menuListAdmin.setBounds(25, 160, 100 ,20);
+        logoutButton = new JButton("Logout");
+        logoutButton.setBounds(25, 300, 100, 20);
+
+        panel.add(menu);
+        panel.add(namaAdmin);
+        panel.add(menuParkir);
+        panel.add(menuListParkir);
+        panel.add(menuListAdmin);
+        panel.add(logoutButton);
+
+        judulLabel = new JLabel("Input Parkir");
+        judulLabel.setBounds(380, 10, 180, 25);
+        platLabel = new JLabel("Plat Nomer : ");
+        platLabel.setBounds(200, 50, 100, 20);
+        platField = new JTextField();
+        platField.setBounds(200, 70, 100, 30);
+
+        tglLabel = new JLabel("Tanggal dan Jam : ");
+        tglLabel.setBounds(200, 100, 100, 20);
+        dateTimePicker = new DateTimePicker();
+        dateTimePicker.setBounds(200, 120, 250, 30);
 
         submitButton = new JButton("Submit");
-        submitButton.setBounds(320, 250, 100, 30);
-        submitButton.setFocusPainted(false);
-        submitButton.addActionListener(this);
+        submitButton.setBounds(200, 160, 100, 30);
 
-        message = new JLabel();
-        message.setBounds(200, 290, 200, 30);
-        message.setFont(new Font("Arial", Font.ITALIC, 12));
+        submitButton.addActionListener((event->{
+            String plat = platField.getText();
+            String tgl = dateTimePicker.getDatePicker().getDateStringOrEmptyString().formatted();
+            String jam = dateTimePicker.getTimePicker().getTimeStringOrEmptyString();
+            String date =  tgl + " " +jam;
 
-        add(titleLabel);
-        add(unameLabel);
-        add(usernameField);
-        add(passLabel);
-        add(passwordField);
+            System.out.println(date);
+
+            boolean cek = parkirController.insert(plat, date, Login.nama);
+            if(cek){
+                JOptionPane.showMessageDialog(this, "Plat sudah ada, input plat lain!");
+            }else{
+                JOptionPane.showMessageDialog(this, "Plat berhasil ditambahkan!");
+                platField.setText("");
+                dateTimePicker.clear();
+            }
+        }));
+        add(panel);
+        add(judulLabel);
+        add(platLabel);
+        add(platField);
+        add(tglLabel);
+        add(dateTimePicker);
         add(submitButton);
-        add(message);
-        add(showPassword);
-
     }
-
 
     public static void main(String[] args) {
         Main main = new Main();
         main.setFrame();
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        if(event.getSource() == submitButton){
-            String username = usernameField.getText();
-            String password = String.valueOf(passwordField.getPassword());
-
-            if(username.equals("admin")){
-                if(password.equals("admin")){
-                    message.setText("");
-                    JOptionPane.showMessageDialog(this, "Login Berhasil !");
-                }
-            }else{
-                message.setText("Username atau Password salah !");
-                message.setForeground(Color.RED);
-            }
-        }
-
-        if(event.getSource() == showPassword){
-            if(showPassword.isSelected()){
-                passwordField.setEchoChar((char) 0);
-            }else{
-                passwordField.setEchoChar('*');
-            }
-        }
     }
 }
